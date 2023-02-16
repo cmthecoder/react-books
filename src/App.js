28ff88030bfcbc0360383a52b29bd9ec0ed9eb21
Chris Mems <christopher.mems@.com>
2023-02-16 10:21:37 -0600

    create routes

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import NavBar from './components/NavBar';
-import './App.css';
 import Footer from './components/Footer';
 import BookList from './components/BookList';
 import Favorites from './components/Favorites';
+import BookDetails from './components/BookDetails';
+import { Routes, Route } from 'react-router-dom';
+import './App.css';
 
 function App() {
   return (
     <div className="">
-      <NavBar />
-      <BookList />
-      <Favorites />
-      <Footer />
+      {/* <NavBar /> */}
+      <Routes>
+        <Route path='/' element={<BookList />} />
+        <Route path='/book/:id' element={<BookDetails />} />
+        <Route path='/favorites' element={<Favorites />} />
+      </Routes>
+      {/* <Footer /> */}
     </div>
   );
 }
