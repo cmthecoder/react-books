3f35b9c1377d3ea2e46c3d7a80b255916ca66a04
Chris Mems <christopher.mems@.com>
2023-02-16 10:12:31 -0600

    create components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import NavBar from './components/NavBar';
 import './App.css';
+import Footer from './components/Footer';
+import BookList from './components/BookList';
+import Favorites from './components/Favorites';
 
 function App() {
   return (
     <div className="">
       <NavBar />
+      <BookList />
+      <Favorites />
+      <Footer />
     </div>
   );
 }
