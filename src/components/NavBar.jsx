92256440d57a68c2b58a300078ffd2b1fb03c069
Chris Mems <christopher.mems@.com>
2023-02-16 10:06:09 -0600

    create navbar component

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const NavBar = () => {
+  return (
+    <div className='navbar'>
+      <div>
+        <h1>React Books App</h1>
+      </div>
+      <div>Favorites</div>
+    </div>
+  )
+}
+
+export default NavBar
